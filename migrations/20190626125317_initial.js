add2c70fe11982caf206edb4d774c38c9d5d4115
Max Silver <Maxbsilver@gmail.com>
2019-06-26 13:42:24 -0600

    fix migration error

--- migrations/20190626125317_initial.js
+++ migrations/20190626125317_initial.js
@@ -1,8 +1,43 @@
-
 exports.up = function(knex) {
-  
+  return Promise.all([
+    knex.schema.createTable("missedConnections", function(table) {
+      table.increments("id").primary();
+      table.string("title");
+      table.string("date");
+      table.string("time");
+      table.string("link");
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable("gigs", function(table) {
+      table.increments("id").primary();
+      table.string("title");
+      table.string("date");
+      table.string("time");
+      table.string("link");
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable("activities", function(table) {
+      table.increments("id").primary();
+      table.string("title");
+      table.string("date");
+      table.string("time");
+      table.string("link");
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable("lostAndFound", function(table) {
+      table.increments("id").primary();
+      table.string("title");
+      table.string("date");
+      table.string("time");
+      table.string("link");
+      table.timestamps(true, true);
+    })
+  ]);
 };
 
 exports.down = function(knex) {
-  
+  return Promise.all([
+    knex.schema.dropTable("missedConnections"),
+    knex.schema.dropTable("gigs")
+  ]);
 };
